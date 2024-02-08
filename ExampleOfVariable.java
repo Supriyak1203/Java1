59402e8080d323c2d88cb4358a4ae9ebda961cb8
Supriyakale1203 <159032773+Supriyakale1203@users.noreply.github.com>
2024-02-08 11:23:57 +0530

    Update ExampleOfVariable.java

---
+++ ExampleOfVariable.java
@@ -0,0 +1,10 @@
+class ExampleOfVariable
+{
+   short priceOfBiryani=120;
+    String  nameOfFood="Biryani";
+    public static void main(String[] args)
+  {
+     short priceOfDosa=80;
+     
+    }
+}

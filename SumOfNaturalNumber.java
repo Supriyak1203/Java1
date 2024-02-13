32f26244837188db809c31dc251668c7f70cf328
Supriyakale1203 <159032773+Supriyakale1203@users.noreply.github.com>
2024-02-13 23:46:39 +0530

    Add files via upload

---
+++ SumOfNaturalNumber.java
@@ -0,0 +1,17 @@
+
+class SumOfNaturalNumber
+{
+public static void main(String[] args)
+{
+int n=20;
+//formula sum=n*(n+1)/2
+int sum=n*(n+1)/2;
+boolean result=sum%2==0;
+System.out.println("sum of number:"+sum);
+System.out.println(sum%2==0);
+}
+}
+//output 
+compile time-sucess
+runtime-sum of number:210
+true

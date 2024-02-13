b8fac8ae0cf147fb144ead318742de6ed6424619
Supriyakale1203 <159032773+Supriyakale1203@users.noreply.github.com>
2024-02-13 23:23:02 +0530

    CompoundOP.java

--- CompoundOP.java
+++ CompoundOP.java
@@ -4,8 +4,12 @@ public static void main(String [] args)
 {
 int a=150;
 int a1=60;
-//a=a*a1
+//a=a*a1;
+//System.out.println(a);  
 a *= a1;
 System.out.println(a);
 }
 }
+//output
+complile time-success
+  Runtime-9000

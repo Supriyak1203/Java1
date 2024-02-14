dba9de4e26386d2b1b777f1bb96548b1baa916ad
Supriyakale1203 <159032773+Supriyakale1203@users.noreply.github.com>
2024-02-14 23:28:58 +0530

    TernaryOperatorOfThreeNumber.java

--- TernaryOperatorOfThreeNumber.java
+++ TernaryOperatorOfThreeNumber.java
@@ -5,6 +5,7 @@ public static void main(String []args)
 int a=20;
 int b=40;
 int c=60;
+  //check which condition is true
 int d=a>b?(b>c?b:c):(a>c?a:c);
 System.out.println("Greatest Number is:"+n);
 }

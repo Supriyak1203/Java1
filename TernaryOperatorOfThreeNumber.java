970ba9d3c206ff23486982071ab4b4e2b793dcb8
Supriya12 <159032773+Supriyakale1203@users.noreply.github.com>
2024-02-14 23:05:03 +0530

    Add files via upload

---
+++ TernaryOperatorOfThreeNumber.java
@@ -0,0 +1,16 @@
+class TernaryOperatorOfThreeNumber
+{
+public static void main(String []args)
+{
+int a=20;
+int b=40;
+int c=60;
+int d=a>b?(b>c?b:c):(a>c?a:c);
+System.out.println("Greatest Number is:"+n);
+}
+}
+//output
+Compile time-success
+Runtime-Greatest Number is:60
+
+

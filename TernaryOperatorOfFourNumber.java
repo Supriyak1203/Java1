217ae1afff0735756e57c587a6f15e70db54ac2c
Supriyak1203 <159032773+Supriyak1203@users.noreply.github.com>
2024-02-15 23:19:44 +0530

    Update TernaryOperatorOfFourNumber.java

--- TernaryOperatorOfFourNumber.java
+++ TernaryOperatorOfFourNumber.java
@@ -7,7 +7,7 @@ int b=40;
 int c=60;
 int d=80;
 //check which condition is true
-int s=a>b?(b>c?b:c):(a>d?a:d);
+int s=(a>b)?((a>c)?((a>d)?a:d):((c>d)?(c):(d))):((b>c)?((b>d)?b:d):((c>d)?c:d));
 System.out.println("Greatest Number is:"+s);
 }
 }

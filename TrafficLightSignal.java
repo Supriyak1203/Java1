08a043211420d29b118ff352863567f31f629054
Supriyak1203 <159032773+Supriyak1203@users.noreply.github.com>
2024-03-01 13:51:08 +0530

    else if

---
+++ TrafficLightSignal.java
@@ -0,0 +1,28 @@
+
+class TrafficLightSignal
+{
+public static void main(String[]args)
+{
+char colour='R';
+if(colour=='R')
+{
+System.out.println("They must be Stop!");
+}
+else if(colour=='Y')
+{
+System.out.println("They must Slow Down!");
+}
+else if(colour=='G')
+{
+System.out.println("They can Go ahead! ");
+}
+else
+{
+System.out.println("Invalid colour");
+} 
+}
+}
+
+// output:
+compile  time:-Sucess
+Run time:-They must be Stop!

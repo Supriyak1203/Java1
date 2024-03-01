3620224cffa945afbabd79945d65e0e71eef65a5
Supriyak1203 <159032773+Supriyak1203@users.noreply.github.com>
2024-03-01 09:40:40 +0530

    if

---
+++ GradeIf.java
@@ -0,0 +1,27 @@
+class GradeIf
+{
+public static void main(String[]args)
+{
+int marks=85;
+if(marks>=80 && marks<=100)
+{
+System.out.println("A+");
+}
+if(marks>=60 && marks<80)
+{
+System.out.println("B+");
+}
+if(marks>=40 && marks<60)
+{
+System.out.println("C+");
+}
+if(marks>=35 && marks<40)
+{
+System.out.println("P+");
+}
+if(marks>=0 && marks<35)
+{
+System.out.println("F+");
+}
+}
+}

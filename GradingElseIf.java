06a0e91269f40731a2df2f6f6eb2ff0c7130faa9
Supriyak1203 <159032773+Supriyak1203@users.noreply.github.com>
2024-03-01 09:41:40 +0530

    else if

---
+++ GradingElseIf.java
@@ -0,0 +1,41 @@
+class GradingElseIf
+{
+public static void main(String []args)
+{
+int marks=70;
+if(marks>=80 && marks<=100)
+{
+System.out.println("A+");
+
+}
+else if(marks>=60 && marks<80)
+{
+System.out.println("B+");
+
+}
+else if(marks>=40 && marks<60)
+{
+System.out.println("C+");
+
+}
+else if(marks>=35 && marks<40)
+{
+System.out.println("P+");
+
+}
+else
+
+{
+System.out.println("F");
+
+}
+
+}
+}
+
+
+
+
+
+
+

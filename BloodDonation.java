3e1e78c45813324e510de070e86f85dafaeaa930
Supriyak1203 <159032773+Supriyak1203@users.noreply.github.com>
2024-03-01 09:39:41 +0530

    if else

---
+++ BloodDonation.java
@@ -0,0 +1,46 @@
+class BloodDonation
+{
+public static void main(String []args)
+{
+int homoglobin=13;
+int age=25;
+boolean disease=false;
+boolean drugs=false;
+if(age>=18 && age<=60)
+{
+if(homoglobin>=13)
+{
+if(disease==false)
+{
+if(drugs==false)
+{
+System.out.println("you are eligible for blood donation");
+}
+else
+{
+System.out.println("you can not donat you are drugs addict");
+}
+}
+else
+{
+System.out.println("you can not donat you have disease");
+}
+}
+else
+{
+System.out.println("you can not donat your hemoglobin is less than 13");
+}
+}
+else
+{
+System.out.println("you are not eligible for blood donation  ");
+}
+}
+}
+
+
+
+
+
+
+

bc989b760bd60c98d2eea1e0707d99af568509fc
Supriyak1203 <159032773+Supriyak1203@users.noreply.github.com>
2024-03-02 10:03:05 +0530

    vowels and consonant

---
+++ VowelsSwitch.java
@@ -0,0 +1,26 @@
+class VowelsSwitch
+{
+public static void main(String [] args)
+{
+char vowels='O';
+switch(vowels)
+{
+case 'A':System.out.println("vowel");
+break;
+case 'E':System.out.println("vowel");
+break;
+case 'i':System.out.println("vowel");
+break;
+case 'O':System.out.println("vowel");
+break;
+case 'U':System.out.println("vowel");
+break;
+default:System.out.println("consonant");
+}
+}
+}
+
+//output
+Compile time-success
+Run time-
+vowel

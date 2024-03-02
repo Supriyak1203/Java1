3feb7c69d749e0b450f048279ad52398bfccbfc7
Supriyak1203 <159032773+Supriyak1203@users.noreply.github.com>
2024-03-02 09:12:41 +0530

    By using break

---
+++ ExampleSwitch2.java
@@ -0,0 +1,29 @@
+class ExampleSwitch2
+{
+public static void main(String[]args)
+{
+int daynum=5;
+switch(daynum)
+{
+case 1:System.out.println("Monday");
+break;
+case 2:System.out.println("Tuesday");
+break;
+case 3:System.out.println("wednesday");
+break;
+case 4:System.out.println("Thursday");
+break;
+case 5:System.out.println("Friday");
+break;
+case 6:System.out.println("Saturday");
+break;
+case 7:System.out.println("Sunday");
+break;
+default:System.out.println("invalid");
+}
+}
+}
+//output:-
+compile time-success
+Run time-success
+Friday

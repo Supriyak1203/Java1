035f83fffe47e134a9cd7f959ead6ce66abb7c36
Supriyak1203 <159032773+Supriyak1203@users.noreply.github.com>
2024-03-09 11:05:38 +0530

    while loop

---
+++ PrimeNumber.java
@@ -0,0 +1,22 @@
+class PrimeNumber
+{
+public static void main(String []args)
+{
+int a=13;
+int b=2;
+int c=0;
+while(b<0)
+{
+if(a%b==0)
+{
+c++;
+}
+b++;
+}
+if(c==0)
+{
+System.out.println("is a prime number");
+}
+}
+
+}

e081f8029979fe5a70729df87b10dde72d537084
Supriyak1203 <159032773+Supriyak1203@users.noreply.github.com>
2024-04-21 22:49:09 +0530

    pattern program

---
+++ Pattern12.java
@@ -0,0 +1,22 @@
+class Pattern12 {
+	public static void main(String[]args) {
+		for(int i=1; i<=3; i++) {
+		   for(int j=1; j<=3; j++) {
+		      if(i==3 || j==3 ||  j==3-i+1) {
+			  System.out.print("*");
+		      }
+			else {
+			  System.out.print(" ");
+		        }
+		   }
+		    System.out.println();
+		}
+	}	
+}
+/* output
+compile time-success
+run time-
+  *
+ **
+***
+*/
---
+++ Pattern15.java
@@ -0,0 +1,23 @@
+class Pattern15 {
+	public static void main(String[]args) {
+		for(int i=1;i<=3;i++) {
+			for(int j=1;j<=3;j++) {
+				if(i==1 || j==3 || j==i) {
+					System.out.print("*");
+				}
+					else {
+						System.out.print(" ");
+					}
+			}
+				System.out.println();
+		}
+	}
+}
+ /* output
+compile time-success
+run time-
+***
+ **
+  *
+
+  */
